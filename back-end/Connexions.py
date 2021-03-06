bbe5dfc874f1fe4dba109800ab368b7fca03c48e
seiffet <seifeddinfettouhi@gmail.com>
2021-03-06 13:10:46 +0100

    new connection plugin testing if network is on

--- back-end/Connexions.py
+++ back-end/Connexions.py
@@ -1,5 +1,10 @@
 import DBController as db
-
+import socket
+import time
+import datetime
+import os
+import sys
+     
 
 class Connexion:
     def __init__(self, ip1, ip2):
@@ -57,3 +62,73 @@ def CreatConnexions():
 			db.insertCons(Connextions)
 
 CreatConnexions()
+
+
+    LOG_FNAME = "network.log"
+    FILE = os.path.join(os.getcwd(), LOG_FNAME)
+
+    #by default test internet connexion to the addresse 1.1.1.1 port 53
+    def send_ping_request(host="1.1.1.1", port=53, timeout=3):
+        try:
+            socket.setdefaulttimeout(timeout)
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((host,port))
+        except OSError as error:
+            return False
+        else:
+            s.close()
+            return True
+    def write_permission_check():
+        try:
+            with open(FILE, "a") as file:
+                pass
+        except OSError as error:
+            print("Log file creation failed")
+            sys.exit()
+        finally:
+            pass
+    def calculate_time(start, stop):
+        time_difference = stop - start
+        seconds = float(str(time_difference.total_seconds()))
+        return str(datetime.timedelta(seconds=seconds)).split(".")[0]
+    def my_net_connection(host="1.1.1.1", port=53, timeout=3,ping_freq=2):
+        monitor_start_time = datetime.datetime.now()
+        motd = "Network connection monitoring started at: " + str(monitor_start_time).split(".")[0] + " Sending ping request in " + str(ping_freq) + " seconds"
+        print(motd)
+     
+        with open(FILE, "a") as file:
+            file.write("\n")
+            file.write(motd + "\n")
+        while True:
+            if send_ping_request():
+                time.sleep(ping_freq)
+            else:
+                down_time = datetime.datetime.now()
+                fail_msg = "Network Connection Unavailable at: " + str(down_time).split(".")[0]
+                print(fail_msg)
+                with open(FILE, "a") as file:
+                    file.write(fail_msg + "\n")
+                    i = 0
+                while not send_ping_request(host, port, timeout):
+                    time.sleep(1)
+                    i += 1
+                    if i >= 3600:
+                        i = 0
+                        now = datetime.datetime.now()
+                        continous_message = "Network Unavailabilty Persistent at: " + str(now).split(".")[0]
+                        print(continous_message)
+                        with open(FILE, "a") as file:
+                            file.write(continous_message + "\n")
+                up_time = datetime.datetime.now()
+                uptime_message = "Network Connectivity Restored at: " + str(up_time).split(".")[0]
+     
+                down_time = calculate_time(down_time, up_time)
+                _m = "Network Connection was Unavailable for " + down_time
+     
+                print(uptime_message)
+                print(_m)
+     
+                with open(FILE, "a") as file:
+                    file.write(uptime_message + "\n")
+                    file.write(_m + "\n")
+    #my_net_connection()
